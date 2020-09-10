fc8f355beba52440cf8699a3180381d685eb3aa0
stanleyhuang <huang.stan@icloud.com>
2020-09-10 10:35:25 -0400

    re-committing for changes

--- src/components/codebox.tsx
+++ src/components/codebox.tsx
@@ -3,18 +3,11 @@ import { UnControlled as CodeMirror } from 'react-codemirror2';
 import '../../node_modules/codemirror/mode/javascript/javascript';
 import '../../node_modules/codemirror/lib/codemirror.css';
 import '../../node_modules/codemirror/theme/dracula.css';
-
 import { state } from '../App';
 import { useRecoilValue } from 'recoil';
 
-import JSZip from 'jszip';
-//below lets you download files within your browser
-import FileSaver from 'file-saver';
-
 const CodeBox: React.FC = () => {
   const data = useRecoilValue(state);
-
-  //below will download all the files
 
   return (
     <div className="wholeCodeBox">
