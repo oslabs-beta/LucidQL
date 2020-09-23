c0c637c01944e97089984479c4fe99183bfcc420
Stanley Huang <stanleyhuang@stanleys-mbp.myfiosgateway.com>
2020-09-23 14:17:45 -0400

    fixing jest errors

--- jest-setup.js
+++ jest-setup.js
@@ -1,7 +1,3 @@
 module.exports = async () => {
-<<<<<<< HEAD
   global.testServer = await require('./server/server.js');
-=======
-  global.testServer = await require('./server');
->>>>>>> 23343869d07fc49fac916cee1ab7d7299e9dbeae
 };
